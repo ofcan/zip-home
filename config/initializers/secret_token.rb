38c4260fa216eff91c15442bc89cb46ef7a814e0
Sven <sven.dupli@gmail.com>
2013-04-05 15:01:02 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZipHome::Application.config.secret_token = '348af9e347b6e640ace405cef950ef0ab156ef05190f2b2c0b34b95f5e8b81533772f027526de34b39ee9bc489dd15e9c593f18b962fd7466f56d9acd5c7a458'
