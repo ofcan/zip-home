dc75290ec1a217a99a9a3b52f9eef83dd77af718
Sven <sven.dupli@gmail.com>
2013-04-06 13:12:01 +0200

    Basic Devise config with basic devise User

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ ZipHome::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Default url for our development mail
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ZipHome::Application.routes.draw do
 
+  devise_for :users
+
   root :to => 'pages#index'
 
 end
