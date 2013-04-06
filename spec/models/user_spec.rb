f93a11e3d24bc1935051ac94d2107d2448c4bf44
Sven <sven.dupli@gmail.com>
2013-04-06 13:22:03 +0200

    Added basic tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { FactoryGirl.build :user }
+
+  it 'creates new user' do
+    user.should be_valid
+  end
+
+  it 'responds to proper methods' do
+    user.should respond_to :email
+    user.should respond_to :password
+  end
+
 end
