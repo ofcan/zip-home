b0b9d18b0a69de22b728142b8d876baf63a29093
Sven <sven.dupli@gmail.com>
2013-04-07 19:18:28 +0200

    Basic form for Posts in new and edit actions

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -5,6 +5,7 @@ describe "Posts" do
   let!(:admin) { create :admin }
   let!(:user) { create :user }
   let!(:blog_post) { create :post }
+  let!(:valid_attributes) { attributes_for :post }
   
   context 'success' do
 
@@ -34,6 +35,25 @@ describe "Posts" do
       current_path.should == edit_post_path(blog_post)
     end
 
+    it 'should create new post' do
+      sign_in_as!(admin)
+      visit new_post_path
+      fill_in 'post_title', :with => valid_attributes[:title]
+      fill_in 'post_body', :with => valid_attributes[:body]
+    end
+
+    it 'should update post' do
+      sign_in_as!(admin)
+      visit edit_post_path(blog_post)
+      fill_in 'post_title', :with => valid_attributes[:title]
+      fill_in 'post_body', :with => valid_attributes[:body]
+      click_button 'Update'
+      blog_post.reload
+      blog_post.title.should == valid_attributes[:title]
+      blog_post.body.should == valid_attributes[:body]
+      page.should have_content 'Post updated.'
+    end
+
     it 'should delete post' do
       sign_in_as!(admin)
       visit posts_path
@@ -57,6 +77,13 @@ describe "Posts" do
       visit new_post_path
       current_path.should == posts_path
       page.should have_content "You can't do that."
+    end
+
+    it 'should not see links to delete and edit post on post index' do
+      sign_in_as!(user)
+      visit posts_path
+      page.should_not have_link "edit_post_#{blog_post.id}"
+      page.should_not have_link "delete_post_#{blog_post.id}"
     end
 
   end
