2adda5de60df4511e686255dbfac53baafb2b0bc
Sven <sven.dupli@gmail.com>
2013-04-07 16:10:40 +0200

    Planted basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Users
+user_1 = User.create( username: 'ofca',
+                      email: 'sven.dupli@gmail.com',
+                      password: 'supersecret')
+
+# Posts
+post_1 = Post.new
+post_1.title = 'Proslava useljenja novih ZIP startupa!'
+post_1.body = "Sutra, 02.04. u ZIP.Factoryju imamo cjelodnevno slavlje povodom sluzbenog useljenja 7 novih startup timova."
+post_1.user = user_1
+post_1.save
+
+post_2 = Post.new
+post_2.title = 'ZIP DrinkUp with the Startup Sauna crew @ The Movie Pub'
+post_2.body = 'This week Starup Sauna guys are coming!'
+post_2.user = user_1
+post_2.save
