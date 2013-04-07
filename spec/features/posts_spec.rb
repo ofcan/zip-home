d08651db3e5999b357b9c274721eeb7989a9a878
Sven <sven.dupli@gmail.com>
2013-04-07 17:42:57 +0200

    Basic post show

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -2,10 +2,33 @@ require 'spec_helper'
 
 describe "Posts" do
   
-  it 'should get to posts index from home page' do
-    visit root_path
-    click_link 'Blog'
-    current_path.should == posts_path
+  let!(:blog_post) { FactoryGirl.create :post }
+  
+  context 'success' do
+
+    it 'should get to posts index from home page' do
+      visit root_path
+      click_link 'Blog'
+      current_path.should == posts_path
+    end
+
+    it 'should get to post show page if clicked on post title on post index' do
+      visit posts_path
+      click_link "post_#{blog_post.id}"
+      current_path.should == post_path(blog_post)
+    end
+
+  end 
+
+  context 'failure' do
+
+    it 'should not get to new post from posts index if user not admin' do
+      visit posts_path
+      click_link 'new_post'
+      current_path.should == posts_path
+      page.should have_content "You can't do that."
+    end
+
   end
 
 end
