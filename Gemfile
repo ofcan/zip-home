caec643ec7566ad9428ce094244a7b2f8e23a911
Sven <sven.dupli@gmail.com>
2013-04-07 22:59:48 +0200

    Added markdown support

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.13'
 gem 'pg'
 gem 'devise'
 gem 'simple_form'
+gem 'redcarpet'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(:hard_wrap => true)
+    options = { :no_intra_emphasis => true,
+                :fenced_code_blocks => true,
+                :autolink => true,
+                :space_after_headers => true }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
+  
 end
