cce7f8e5bf5bdc299c88f800a809d949ab38f4fe
Sven <sven.dupli@gmail.com>
2013-04-25 22:03:51 +0200

    Design - partners path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
     @advisors = User.where(:role => 'advisor').all
     @mentors = User.where(:role => 'mentor').all
     @team_members = User.where(:role => 'zip_team_member').all
+    @basic_users = User.where(:role => 'basic').all
   end
 
   def show
