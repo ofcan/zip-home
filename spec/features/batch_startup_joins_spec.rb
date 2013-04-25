e53f82cc0de17849cd49f426466ccee5d33e3aaf
Sven <sven.dupli@gmail.com>
2013-04-25 17:44:26 +0200

    Adding Startups to batches on batch edit path

---
+++ spec/features/batch_startup_joins_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'BatchStartupJoins' do
+  
+  let!(:founder) { create :founder }
+  let!(:batch) { create :batch }
+  let!(:startup) { create :startup }
+  let!(:batch_startup_join) { create :batch_startup_join }
+
+  context 'success' do
+    
+    it 'founder should add startup to batch' do
+      sign_in_as!(founder)
+      visit edit_batch_path(batch)
+      fill_in 'search', :with => startup.name
+      click_button 'Search'
+
+      current_path.should == edit_batch_path(batch)
+      page.should have_content startup.name
+      expect {
+        click_link "create_batch_startup_join_with_startup_#{startup.id}"
+      }.to change(BatchStartupJoin, :count).by(1)
+
+      BatchStartupJoin.last.batch.should == batch
+      BatchStartupJoin.last.startup.should == startup
+    end
+  end
+
+  context 'failure' do
+  end
+
+end
