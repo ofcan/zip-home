4ae57544399d452d53068eea412d20fdc0b93721
Sven <sven.dupli@gmail.com>
2013-04-26 14:55:22 +0200

    Added truncate_by_paragraph and remove_images helpers in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,18 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, options).render(text).html_safe
   end
 
+  def truncate_by_paragraph(text, num=1)
+    # since I'm not sure if the text will have \r, \n or both I'm swapping
+    # all \r characters for \n, then splitting at the newlines and removing
+    # any blank characters from the array
+    array = text.gsub("\r", "\n").split("\n").reject { |i| i == "" }
+    array.take(num).join("\n")
+  end
+
+  def remove_images(text)
+    text.gsub(/<img([^>])+/, "")
+  end
+
   def gravatar_picture(user, size)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?d=identicon&size=#{size}"
