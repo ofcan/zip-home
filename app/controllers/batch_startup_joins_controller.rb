2a216c95e4385f3963732fb7d47ee32dce7730f2
Sven <sven.dupli@gmail.com>
2013-04-29 21:40:41 +0200

    Cannot create 2 identical batch_startup_joins

--- app/controllers/batch_startup_joins_controller.rb
+++ app/controllers/batch_startup_joins_controller.rb
@@ -1,14 +1,15 @@
 class BatchStartupJoinsController < ApplicationController
   
   before_filter :find_batch
+  before_filter :find_startup, :only => :create
   before_filter :assert_current_user
   before_filter :assert_admin
   
   def create
     @batch_startup_join = @batch.batch_startup_joins.build
     @batch_startup_join.batch = @batch
-    @batch_startup_join.startup = Startup.find(params[:batch_startup_join][:startup_id])
-    if @batch_startup_join.save
+    @batch_startup_join.startup = @startup
+    if BatchStartupJoin.find_by_batch_id_and_startup_id(@batch, @startup) == nil && @batch_startup_join.save
       redirect_to edit_batch_path(@batch), notice: 'Startup added.'
     else
       redirect_to edit_batch_path(@batch), alert: 'Startup not added.'
@@ -24,6 +25,10 @@ class BatchStartupJoinsController < ApplicationController
 
   def find_batch
     @batch = Batch.find(params[:batch_id])
+  end
+
+  def find_startup
+    @startup = Startup.find(params[:batch_startup_join][:startup_id])
   end
 
 end
